d4ba8c8178fa712566524fc45d79069abd7d4f31
N V Mahendra Varma <nvmvarma2003@gmail.com>
2025-02-27 13:25:55 +0530

    Add files via upload

---
+++ practiceDaoDto1/UserDAO.java
@@ -0,0 +1,38 @@
+package practiceDaoDto1;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserDAO {
+	private List<UserDTO> users= new ArrayList<>();
+	
+	public void addUser(UserDTO user) {
+		users.add(user);
+	}
+	
+	public List<UserDTO> getAllUsers(){
+		return users;
+	}
+	
+	public UserDTO getUserById(int id) {
+		for(UserDTO user : users) {
+			if(user.getId()==id) {
+				return user;
+			}
+		}
+		return null;
+	}
+	
+	public void updateUser(UserDTO user) {
+		for(int i=0;i<users.size();i++) {
+			if(users.get(i).getId()==user.getId()) {
+				users.set(i,user);
+				break;
+			}
+		}
+	}
+	
+	public void deleteUser(int id) {
+		users.removeIf(user -> user.getId()==id);
+	}
+}
