a525d1126df706cc3a9b10ca70a9c1309ee76319
N V Mahendra Varma <nvmvarma2003@gmail.com>
2025-02-27 13:32:26 +0530

    Update MainApp.java

--- practiceDaoDto1/MainApp.java
+++ practiceDaoDto1/MainApp.java
@@ -1,25 +1,23 @@
 package practiceDaoDto1;
 
-import practiceDaoDto1.UserDAO;
-import practiceDaoDto1.UserDTO;
 public class MainApp {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		UserDAO userDAO = new UserDAO();
-		
+			
 		userDAO.addUser(new UserDTO(1, "nvm varma", "varma@example.com"));
-        userDAO.addUser(new UserDTO(2, "tvv raana", "ramana@mail.com"));
-        
-        System.out.println("All Users: " + userDAO.getAllUsers());
-        
-        System.out.println("user with id 1 :" +userDAO.getUserById(1));
-        
-        userDAO.updateUser(new UserDTO(2,"tvv ramana", "ramana@mail.com"));
-        System.out.println("updated user : "+userDAO.getUserById(2));
-        
-        userDAO.deleteUser(2);
-        System.out.println("after deletion : "+userDAO.getAllUsers());
+	        userDAO.addUser(new UserDTO(2, "tvv raana", "ramana@mail.com"));
+	        
+	        System.out.println("All Users: " + userDAO.getAllUsers());
+	        
+	        System.out.println("user with id 1 :" +userDAO.getUserById(1));
+	        
+	        userDAO.updateUser(new UserDTO(2,"tvv ramana", "ramana@mail.com"));
+	        System.out.println("updated user : "+userDAO.getUserById(2));
+	        
+	        userDAO.deleteUser(2);
+	        System.out.println("after deletion : "+userDAO.getAllUsers());
 
 
 	}
