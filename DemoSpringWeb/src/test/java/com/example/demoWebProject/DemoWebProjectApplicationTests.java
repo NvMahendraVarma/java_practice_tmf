332435e896009c7bf5b80cec201768e6a0ecb302
N V Mahendra Varma <nvmvarma2003@gmail.com>
2025-04-01 12:07:54 +0530

    Add files via upload

---
+++ DemoSpringWeb/src/test/java/com/example/demoWebProject/DemoWebProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.demoWebProject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DemoWebProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
